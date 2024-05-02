b6491ca81c2c07b8304879976e349264c481b97f
Rajbeer Singh <rajbeersingh@Rajbeers-MacBook-Air.local>
2024-05-02 16:10:55 +0530

    dd

--- helpers/axios.ts
+++ helpers/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3500',
+  baseURL: 'https://sih-backend-production.up.railway.app',
 });
 
 export default axiosClient;
